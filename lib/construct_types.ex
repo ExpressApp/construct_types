7de69c78be406a397bbb55b4c4b0c493d5aace13
Yuri Artemev <i@artemeff.com>
2018-02-03 10:03:11 +0300

    init

---
+++ lib/construct_types.ex
@@ -0,0 +1,18 @@
+defmodule ConstructTypes do
+  @moduledoc """
+  Documentation for ConstructTypes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConstructTypes.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/construct_types_test.exs
@@ -0,0 +1,8 @@
+defmodule ConstructTypesTest do
+  use ExUnit.Case
+  doctest ConstructTypes
+
+  test "greets the world" do
+    assert ConstructTypes.hello() == :world
+  end
+end
