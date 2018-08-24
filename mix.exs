351990949e08d342e028862a7c40cc8174b03df1
Yuri Artemev <i@artemeff.com>
2018-08-24 16:09:36 +0300

    add hex package meta

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Construct.Types.MixProject do
       version: "0.1.0",
       elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: description(),
+      package: package(),
+
+      # Docs
+      name: "Construct.Types",
+      docs: docs()
     ]
   end
 
@@ -19,7 +27,29 @@ defmodule Construct.Types.MixProject do
 
   defp deps do
     [
-      {:construct, "~> 1.0"}
+      {:construct, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    "Types for Construct"
+  end
+
+  defp package do
+    [
+      name: :construct_types,
+      maintainers: ["Yuri Artemev"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ExpressApp/construct_types"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: "https://github.com/ExpressApp/construct_types",
+      extras: ["README.md"]
     ]
   end
 end
