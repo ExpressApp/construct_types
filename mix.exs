ad3b037cdc2384f3ecf6e7ab01f8aa59800b5153
Yuri Artemev <i@artemeff.com>
2018-08-24 08:46:08 +0300

    remove boilerplate

--- mix.exs
+++ mix.exs
@@ -1,28 +1,25 @@
-defmodule ConstructTypes.MixProject do
+defmodule Construct.Types.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :construct_types,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:construct, "~> 1.0"}
     ]
   end
 end
